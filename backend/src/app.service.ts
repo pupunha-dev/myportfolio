39d136a3fcb8aefbe905e16daa0415db3dee5eb9
PupunhaDev <pupunha.dev@gmail.com>
2025-03-18 19:10:18 -0300

    Backend criado e prisma instalado

---
+++ backend/src/app.service.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import {x} from "@core";
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!' + x;
+  }
+}
